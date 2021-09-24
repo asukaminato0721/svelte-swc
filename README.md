b34d5408b8fa41b9fc217aeaeb09da926e4751b3
wuyudi <wuyudi1109@gmail.com>
2021-09-25 02:50:14 +0800

    feat: :tada: init template

---
+++ README.md
@@ -0,0 +1,3 @@
+# Svelte-swc
+
+Use Standard Svelte with [sourceMaps](https://swc.rs/docs/configuring-swc#sourcemaps) with [rollup-plugin](https://github.com/mentaljam/rollup-plugin-swc)
---
+++ src/App.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+</script>
+
+<main>
+  <p>A Template with swc</p>
+</main>
+
+<style>
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
+
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
+</style>
